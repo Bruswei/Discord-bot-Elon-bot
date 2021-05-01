fbc3838986c5ba1a43e53bacabc34e374324cdca
Yijun <yijun@staut.land>
2021-05-01 12:32:48 +0200

    Add changeGuildName function

--- bot.js
+++ bot.js
@@ -11,11 +11,19 @@ client.on('ready', () => {
         console.log(`Bot is logged on ${guild.name} with id ${guild.id}`);
     });
 
-    client.guilds.fetch("155409166859632640", false, false)
-    .then(guild => {
-       return guild.setName("TSLA: 709.44, BTC: 57,454");
-    })
-    .then(console.log);
+    const name = "TSLA: 710,00 & BTC: 57454";
+    changeGuildName(name);
+
 });
 
 client.login(process.env.BOT_TOKEN);
+
+function changeGuildName(name) {
+    client.guilds.fetch("155409166859632640", false, false)
+    .then(guild => {
+       return guild.setName(name);
+    })
+    .then(guild => {
+        console.log(`Server name set to ${guild.name}`)
+    })
+}
