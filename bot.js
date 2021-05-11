60ba4d299739f99525785a85efd667bc6a1cdd78
Yijun <yijun@staut.land>
2021-05-11 23:13:11 +0200

    Add doge coin and update bitcoin api to bots

--- bot.js
+++ bot.js
@@ -2,20 +2,33 @@ require('dotenv').config()
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fetch = require("node-fetch");
 var guild;
 
+const yahooStockPrice = require('yahoo-stock-prices')
 
 client.on('ready', () => {
-    guild = client.guilds.cache;
-    gName = guild.map(guild => {
+    guild = client.guilds.fetch("155409166859632640", false, false);
+    gName = guild.then(guild => {
         console.log(`Bot is logged on ${guild.name} with id ${guild.id}`);
     });
 
     // function runs every 10 sec
     setInterval(() => {
-        const name = "TSLA: 710,00 & BTC: 57454 & GME: 174.40";
-        changeGuildName(name);
-    }, 10000);
+        // Fetching stock prices
+        const tsla = yahooStockPrice.getCurrentPrice('TSLA');
+        const btc = fetch('https://blockchain.info/tobtc?currency=USD&value=500')
+            .then(response => response.text());
+        const doge = fetch('https://sochain.com//api/v2/get_price/DOGE/USD')
+            .then(response => response.text())
+            .then(res => JSON.parse(res));
+        Promise.all([tsla, btc, doge]).then(price => {
+                var doge = parseFloat(price[2].data.prices[0].price);
+                
+                var name = `TSLA: ${price[0]} BTC: ${(500/price[1]).toFixed(2)} DOGE: ${doge.toFixed(2)}`;
+                changeGuildName(name);
+        });
+    }, 5000);
 
 });
 
@@ -28,5 +41,5 @@ function changeGuildName(name) {
     })
     .then(guild => {
         console.log(`Server name set to ${guild.name}`)
-    })
+    });
 }
