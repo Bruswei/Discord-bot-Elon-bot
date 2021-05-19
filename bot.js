032d6fb0849f98b32befb8c40f058092a152c956
Yijun <yijun@staut.land>
2021-05-19 22:19:36 +0200

    Add new name schema and logic to update

--- bot.js
+++ bot.js
@@ -7,6 +7,15 @@ var guild;
 
 const yahooStockPrice = require('yahoo-stock-prices')
 
+var tsla = 0;
+var btc = 0;
+var doge = 0;
+
+// Which price was updated last
+// 1 - tesla, 2 - btc, 3 - doge
+var lastPrice = 1;
+
+
 client.on('ready', () => {
     guild = client.guilds.fetch("155409166859632640", false, false);
     gName = guild.then(guild => {
@@ -15,10 +24,25 @@ client.on('ready', () => {
 
     // function runs every 10 sec
     setInterval(() => {
-        getPromises().then(price => {
-                var name = `TSLA: ${price[0]} BTC: ${price[1]} DOGE: ${price[2]}`;
-                changeGuildName(name);
-        });
+        updatePrices();
+        switch(lastPrice) {
+            case 1:
+                changeGuildName(`Dogefather club: TSLA - ${tsla}$`)
+                lastPrice++;
+                break;
+
+            case 2: 
+                changeGuildName(`Dogefather club: BTC - ${btc}$`)
+                lastPrice++;
+                break;
+
+            case 3: 
+                changeGuildName(`Dogefather club: DOGE - ${doge}$`)
+                lastPrice = 1;
+                break;
+            default:
+                lastPrice = 1;
+        }
     }, 5000);
 
 });
@@ -35,16 +59,22 @@ function changeGuildName(name) {
     });
 }
 
-// Function to fetch stock and crypto prices and calculate 
-function getPromises() {
-    const tsla = yahooStockPrice.getCurrentPrice('TSLA');
-    const btc = fetch('https://blockchain.info/tobtc?currency=USD&value=500')
+// Fetch prices and updates global variables
+function updatePrices() {
+    yahooStockPrice.getCurrentPrice('TSLA').then(price => {
+        tsla = price;
+    });
+
+    fetch('https://blockchain.info/tobtc?currency=USD&value=500')
         .then(response => response.text())
-        .then(price => (500/price).toFixed(2));
-    const doge = fetch('https://sochain.com//api/v2/get_price/DOGE/USD')
+        .then(price => {
+            btc = (500/price).toFixed(2);
+        });
+
+    fetch('https://sochain.com//api/v2/get_price/DOGE/USD')
         .then(response => response.text())
         .then(res => JSON.parse(res))
-        .then(res =>  parseFloat(res.data.prices[0].price).toFixed(2));
-
-    return Promise.all([tsla, btc, doge])
+        .then(res => {
+            doge = parseFloat(res.data.prices[0].price).toFixed(2);
+        });
 }
